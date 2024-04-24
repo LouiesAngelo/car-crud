1c1058245e979819e36c1842a8b5ca841d4d3ff5
LouiesAngelo <93696341+LouiesAngelo@users.noreply.github.com>
2024-04-25 00:07:07 +0800

    first commit

--- frontend/src/AppNavbar.js
+++ frontend/src/AppNavbar.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import {Link} from 'react-router-dom';
-..
+
 export default class AppNavbar extends Component {
     constructor(props) {
         super(props);
